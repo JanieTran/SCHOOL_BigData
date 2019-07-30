f4340300a4964a5632f382f7ab4454ac45ede9ba
Janie <emerald1901@gmail.com>
2019-07-30 14:47:09 +0700

    K Means example

---
+++ lectures/example_kmeans.py
@@ -0,0 +1,14 @@
+# import statements
+from sklearn.datasets import make_blobs
+import numpy as np
+import matplotlib.pyplot as plt
+
+# create blobs
+data = make_blobs(n_samples=500, n_features=2, centers=5, cluster_std=1)
+# create np array for data points
+points = data[0]
+# create scatter plot
+plt.scatter(data[0][:, 0], data[0][:, 1], c=data[1], cmap='viridis')
+plt.xlim(-15, 15)
+plt.ylim(-15, 15)
+plt.show()
